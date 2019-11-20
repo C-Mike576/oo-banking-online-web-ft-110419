19f184d23c7bd5db2d26527c8842bde3cb0d2a44
Michael Calkins <m.calkins576@gmail.com>
2019-11-20 17:36:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
       @status = "complete"
     else
       @status = "rejected"
-      print "Transaction rejected. Please check your account balance."
+      p "Transaction rejected. Please check your account balance."
     end  
     
   end
