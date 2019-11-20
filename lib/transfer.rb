8a58cdf6a41418b5be3de671b19e3eb3f6384448
Michael Calkins <m.calkins576@gmail.com>
2019-11-20 17:33:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   
   def execute_transaction
     #binding.pry
-    if @status == "pending" && self.vaild?
+    if @status == "pending" && self.valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
